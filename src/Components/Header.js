6d45b3521a0eb6f11cdc8b0d82a7bb911d954077
brito101 <rodrigobrito101@hotmail.com>
2022-06-27 18:30:13 -0300

    Create Components and UserContext

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,27 @@ import React from "react"
 import { Link } from "react-router-dom"
 import styles from "./Header.module.css"
 import { ReactComponent as Logo } from "../Assets/logo.svg"
+import { UserContext } from "../UserContex"
 
 export default function Header() {
+  const { data, userLogout } = React.useContext(UserContext)
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to='/' aria-label="Moments - Home">
+        <Link className={styles.logo} to='/' aria-label='Moments - Home'>
           <Logo />
         </Link>
-        <Link className={styles.login} to='/login'>Login / Cadastro</Link>
+        {data ? (
+          <Link className={styles.login} to='/conta'>
+            {data.name}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className={styles.login} to='/login'>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   )
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,20 @@
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { UserContext } from "../../UserContex"
 import LoginCreate from "./LoginCreate"
-import Loginform from "./Loginform"
+import LoginForm from "./LoginForm"
 import LoginLost from "./LoginLost"
 import LoginReset from "./LoginReset"
 
 export default function Login() {
+  const { login } = React.useContext(UserContext)
+
+  if (login === true) return <Navigate to="/conta" />;
+
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Loginform />} />
+        <Route path='/' element={<LoginForm />} />
         <Route path='cadastro' element={<LoginCreate />} />
         <Route path='recuperar-senha' element={<LoginLost />} />
         <Route path='resetar-senha' element={<LoginReset />} />
